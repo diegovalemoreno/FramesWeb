1ff6f58bf932e69f639ad29d2f8095cf4f61e084
Diego Moreno <diegosvm@hotmail.com>
2017-03-02 22:25:06 -0300

    Implementação da API de Sumario de pagamentos
    Inclusão da API de sumario de pagamentos.

--- config/routes.js
+++ config/routes.js
@@ -8,4 +8,8 @@ module.exports = function(server) {
     //rotas da API
     const billingCyclesService = require('../api/billingCycle/billingCycleService')
     billingCyclesService.register(router, '/billingCycles')
+
+    const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+    router.route('/billingSummary').get(billingSummaryService.getSummary)
+
 }
